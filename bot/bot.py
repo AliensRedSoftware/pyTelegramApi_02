e9a81acbe99f19ba9248b4b50af4b4cf2a59397a
Melodee <bakmen2543@gmail.com>
2021-10-30 23:19:33 +0000

    Zzzz

--- bot/bot.py
+++ bot/bot.py
@@ -1,18 +1,18 @@
 import bot.classes.pyTelegramApi as api
 
 def main(token):
-	api.pyTelegramApi.setToken(api,token) #Установка токена
+	print(api.msg.id)
+	api.pyTelegramApi.setToken(api, token) #Установка токена
 	api.pyTelegramApi.prefix = '$' #Установка префикса
 	#Команды
-	api.pyTelegramApi.addcommand('str' , 'str.str')
-	api.pyTelegramApi.addcommand('getStickerId' , 'sticker.sticker')
-	api.pyTelegramApi.addcommand('sendSticker' , 'sticker.sendSticker')
-	api.pyTelegramApi.addcommand('art2d' , 'fun.art2d')
+	api.pyTelegramApi.addcommand('str', 'str.str')
+	api.pyTelegramApi.addcommand('getStickerId', 'sticker.sticker')
+	api.pyTelegramApi.addcommand('sendSticker', 'sticker.sendSticker')
+	api.pyTelegramApi.addcommand('art2d', 'fun.art2d')
 	#Меню
-	api.pyTelegramApi.addcommand('start' , 'system.ver')
-	api.pyTelegramApi.addcommand('menu' , 'system.ver')
-	api.pyTelegramApi.addcommand('help' , 'system.ver')
+	api.pyTelegramApi.addcommand('start', 'system.ver')
+	api.pyTelegramApi.addcommand('menu', 'system.ver')
+	api.pyTelegramApi.addcommand('help', 'system.ver')
 	#Парсинг новостей
 	api.pyTelegramApi.addcommand('ixbt' , 'news.ixbt')
-	#Проверка на правильность токена
-	api.pyTelegramApi.checkToken(api.pyTelegramApi.getToken(api)) #Проверка на подлиность токе
+	api.pyTelegramApi.getUpdates(api.pyTelegramApi.getToken(api))
--- bot/classes/pyTelegramApi.py
+++ bot/classes/pyTelegramApi.py
@@ -26,6 +26,7 @@ class InlineKeyBoard:
 		vrm = '"inline_keyboard": [['+item+']]'
 		return vrm
 	def keyboard(self, desc='Описание', items=''): #Возвращаем inline клавиаутуру
+		desc+="\n--\n[about]\n[first_name] =>"
 		chat_id = str(pyTelegramApi.getChatId(self))
 		headers={
 			'Content-Type': 'application/json',
@@ -42,39 +43,50 @@ class InlineKeyBoard:
 		K=InlineKeyBoard.UXBtn({'txt':'❌','func':'kill@'+name}) #Покончить
 		#Ячейки
 		return InlineKeyBoard.UXItem(B+','+H+','+K)
-
+class user:
+	id=0
+class msg:
+	id=0
+	txt='undefined'
 class pyTelegramApi:
 	token=0 #Токен
-	message_id=0 #message id
-	listcommand=[] #Лист комманд
+	message_id=0 #message
 	stickerid=False #Стикер id
 	prefix='/' #Префикс
+	listcommand=[] #Лист комманд
 	def setToken(self, token): #Установка токена
 		self.token = token
-		print ('[Успешно :)] [Токен => {0}]'.format(token))
+		print('[Успешно :)] [Токен => {0}]'.format(token))
+		if	pyTelegramApi.checkToken(pyTelegramApi.getToken(self)) == False:
+			sys.exit()
 	def getToken(self): #Получение токена
 		return 'https://api.telegram.org/bot{0}/'.format(self.token)
-	def checkToken(token): #Проверка токена
-		json_response = pyTelegramApi.request(token , 'getMe')
-		if json_response['ok'] == True:
-			print ('[Успешно :)] [Проверка => OK]')
-			pyTelegramApi.getUpdates(token)
+	def	checkToken(token): #Проверка токена
+		json_response = pyTelegramApi.request(token, 'getMe')
+		if	json_response['ok'] == True:
+			print('[Успешно :)] [Проверка => OK]')
+			return True
 		else:
-			print ('[Ошибка :(] [Проверка => Неверный токен]')
+			print('[Ошибка :(] [Проверка => Неверный токен]')
+			return False
 	def request(token, method, fields = {}): #Запрос к серверу и возвращение
 		https = urllib3.PoolManager(cert_reqs='CERT_REQUIRED',ca_certs=certifi.where())
 		r = https.request('GET', token + method, fields)
-		return json.loads(r.data.decode('utf-8'))
-	def addcommand (name, module): #Добавить команду
+		data=json.loads(r.data.decode('utf-8'))
+		if	data['ok'] == False:
+			return False
+		else:
+			return data
+	def addcommand(name, module): #Добавить команду
 		pyTelegramApi.listcommand.append(pyTelegramApi.prefix+name + '=' + module)
-	def sendMessage_id(self, text = 'Привет', chatid = ''): #отправить сообщение с chatid
+	def sendMessage_id(self, txt = 'Привет', chatid = ''): #отправить сообщение с chatid
 		if chatid == '':
 			chatid = pyTelegramApi.getChatId(self)
-		pyTelegramApi.request(pyTelegramApi.getToken(self), 'sendMessage', {'chat_id' : chatid, 'text' : text})
-	def sendMessage(self, text = 'Привет'): #отправить сообщение без chatid
+		pyTelegramApi.request(pyTelegramApi.getToken(self), 'sendMessage', {'chat_id' : chatid, 'text' : txt})
+	def sendMessage(self, txt = 'Привет'): #отправить сообщение без chatid
 		chatid = pyTelegramApi.getChatId(self)
-		pyTelegramApi.request(pyTelegramApi.getToken(self), 'sendMessage', {'chat_id' : chatid, 'text' : text})
-	def sendPhoto_ByUrl(self , PhotoUrl): #Отправка фото по url адресу
+		pyTelegramApi.request(pyTelegramApi.getToken(self), 'sendMessage', {'chat_id' : chatid, 'text' : txt})
+	def sendPhoto_ByUrl(self, PhotoUrl): #Отправка фото по url адресу
 		chatid = pyTelegramApi.getChatId(self)
 		pyTelegramApi.request(pyTelegramApi.getToken(self), 'sendPhoto', {'chat_id' : chatid, 'photo' : PhotoUrl})
 	def getChatId(self): #Возвращаем chatid от сообщение
@@ -84,35 +96,44 @@ class pyTelegramApi:
 		except:
 			return json_response['result'][0]['callback_query']['message']['chat']['id']
 	def getText(self): #Возвращаем текст
+		return msg.txt
 		json_response = pyTelegramApi.request(pyTelegramApi.getToken(self), 'getUpdates', {'offset' : -1})
-		if json_response['result'][0]['message']['text']:
+		if	json_response['result'][0]['message']['text']:
 			return json_response['result'][0]['message']['text']
 		else:
 			json_response['result'][0]['message']['chat']['text']
-	def sendMessage_array (self, text = ['Привет1', 'Привет2']):
+	def sendMessage_array(self, arr = ['Привет1', 'Привет2']):
 		chatid = pyTelegramApi.getChatId(self)
 		txt = ''
-		for str in text:
+		for str in arr:
 			txt += str + "\n"
 		pyTelegramApi.request(pyTelegramApi.getToken(self), 'sendMessage', {'chat_id' : chatid, 'text' : txt})
-	def getlist (): #Получить список команд
+	def getlist(): #Получить список команд
 		return pyTelegramApi.listcommand
-	def sendSticker (self, StickerId): #Отправить стикер
+	def sendSticker(self, StickerId): #Отправить стикер
 		chatid = pyTelegramApi.getChatId(self)
 		pyTelegramApi.request(pyTelegramApi.getToken(self), 'sendSticker', {'chat_id' : chatid, 'sticker' : StickerId})
-	def getStickerId (): #Возвращаем id стикера
+	def getStickerId(): #Возвращаем id стикера
 		return pyTelegramApi.stickerid
-	def DeleteMessage(self,id):
-		chatid=pyTelegramApi.getChatId(self)
-		pyTelegramApi.request(pyTelegramApi.getToken(self), 'deleteMessage', {'chat_id' : chatid,'message_id': id})
+	def DeleteMessageId(self,id): # del Msg
+		pyTelegramApi.request(pyTelegramApi.getToken(self), 'deleteMessage', {'chat_id' : chatid, 'message_id' : id})
+	def sniffer(json_response):
+		if	json_response['result'][0]['message']['message_id']:
+			#msg
+			msg.id=json_response['result'][0]['message']['message_id']
+			user.id=json_response['result'][0]['message']['from']['id']
+		elif json_response['result'][0]['callback_query']['message']['message_id']:
+			#msg
+			msg.id=json_response['result'][0]['callback_query']['message']['message_id']
+			user.id=json_response['result'][0]['callback_query']['message']['from']['id']
 	def getUpdates(token):#Бесконечный цикл
-		sys.setrecursionlimit(10000) #Кол-во запросов
+		sys.setrecursionlimit(10050) #Кол-во запросов
 		json_response = pyTelegramApi.request(token, 'getUpdates', {'offset' : -1})
 		try:
 			chatid = json_response['result'][0]['message']['chat']['id']
 		except:
 			try:
-				if pyTelegramApi.message_id != json_response['result'][0]['callback_query']['message']['message_id']:
+				if	msg.id != json_response['result'][0]['callback_query']['message']['message_id']:
 					json_response['result'][0]['callback_query']['data']
 					chatid = json_response['result'][0]['callback_query']['message']['chat']['id']
 					message_id = json_response['result'][0]['callback_query']['message']['message_id']
@@ -120,30 +141,33 @@ class pyTelegramApi:
 					execute=func.split('@')[0]
 					func=func.split('@')[1]
 					func=getattr(importlib.import_module(func), execute)
-					pyTelegramApi.request(token, 'deleteMessage', {'chat_id' : chatid,'message_id': message_id})
-					pyTelegramApi.message_id = json_response['result'][0]['callback_query']['message']['message_id']
+					pyTelegramApi.request(token, 'deleteMessage', {'chat_id' : chatid, 'message_id' : message_id})
+					#Нюхаем значение для быстрого хода (HACK)
+					pyTelegramApi.sniffer(json_response)
 					func()
 			except:
 				pass
 			pyTelegramApi.getUpdates(token)
-		if pyTelegramApi.message_id != json_response['result'][0]['message']['message_id']:
-			for text in json_response['result'][0]['message']:
-				if text == 'text':
-					text = json_response['result'][0]['message']['text']
+		if	msg.id != json_response['result'][0]['message']['message_id']:
+			for	txt in json_response['result'][0]['message']:
+				if	txt == 'text':
+					txt = json_response['result'][0]['message']['text']
 					for str in pyTelegramApi.getlist():
 						module = str.split('=')[1]
 						command = str.split('=')[0]
-						text_gen = text.split('@')
-						if command == text_gen[0]:
+						txt_gen = txt.split('@')
+						if	command == txt_gen[0]:
 							print('[Успешно :)] [user] [Модуль выполнен => {0}]'.format(module))
 							importlib.import_module('bot.modules.' + module).main()
-						elif command == text.split(' ')[0]:
+						elif command == txt.split(' ')[0]:
 							print('[Успешно :)] [user] [Модуль выполнен => {0}]'.format(module))
 							importlib.import_module('bot.modules.' + module).main()
-					pyTelegramApi.message_id = json_response['result'][0]['message']['message_id']
+					#Нюхаем значение для быстрого хода (HACK)
+					pyTelegramApi.sniffer(json_response)
 					pyTelegramApi.getUpdates(token)
 					return
-				elif text == 'sticker':
+				elif txt == 'sticker':
 					pyTelegramApi.stickerid = json_response['result'][0]['message']['sticker']['file_id']
-		pyTelegramApi.message_id = json_response['result'][0]['message']['message_id']
+		#Нюхаем значение для быстрого хода (HACK)
+		pyTelegramApi.sniffer(json_response)
 		pyTelegramApi.getUpdates(token)
