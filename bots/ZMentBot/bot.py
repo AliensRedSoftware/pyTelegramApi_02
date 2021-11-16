2a9bde68f45c49426babfff10d2d2f35b96294a1
Melodee <bakmen2543@gmail.com>
2021-11-17 01:04:42 +0900

    Zzzzzzzzzzzzzzzzzzzzzzzzzzzzz

---
+++ bots/ZMentBot/bot.py
@@ -0,0 +1,16 @@
+import classes.pyTelegramApi as api
+
+token		=	'undefined'
+listcommand	=	[]	#Лист комманд
+prefix		=	'/'	#Префикс
+message_ids	=	[]
+cfg			=	{}	# cfg threads...
+
+def main(name):
+	api.pyTelegramApi.setToken(name) #Установка токена
+	api.pyTelegramApi.setPrefix(name, '$') #Установка префикса
+	#Команды
+	
+	#Start proccess threads...
+	api.pyTelegramApi.InitThread(name)
+	api.pyTelegramApi.thread(name)
